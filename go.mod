0c36d3bc65c5aae97c63d83d25c7b4b5ae51bf49
takumi katori <>
2020-10-18 22:43:35 +0900

    update injector

--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/uuid v1.1.2
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	go.uber.org/dig v1.10.0
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/sys v0.0.0-20201017003518-b09fb700fbb7 // indirect
 )
--- injector/injector.go
+++ injector/injector.go
@@ -2,8 +2,14 @@ package injector
 
 import (
 	"github.com/takutakukatokatojapan/image_analysis_api/api/server"
+	"github.com/takutakukatokatojapan/image_analysis_api/api/server/handler"
+	"github.com/takutakukatokatojapan/image_analysis_api/api/server/router"
+	"github.com/takutakukatokatojapan/image_analysis_api/applications"
+	"github.com/takutakukatokatojapan/image_analysis_api/domain/service"
 	"github.com/takutakukatokatojapan/image_analysis_api/infrastructure/config"
+	"github.com/takutakukatokatojapan/image_analysis_api/infrastructure/logger"
 	"go.uber.org/dig"
+	"log"
 )
 
 var c *dig.Container
@@ -11,6 +17,22 @@ var c *dig.Container
 func init() {
 	c = dig.New()
 	_ = c.Provide(config.NewConfig)
+	// setting logger...
+	if err := c.Invoke(func(config config.Config) {
+		logger.NewLogger(config.APPName)
+	}); err != nil {
+		log.Fatal(err)
+	}
+	// service...
+	_ = c.Provide(service.NewAuthService)
+	_ = c.Provide(service.NewFileLoadingServiceImpl)
+	// use case...
+	_ = c.Provide(applications.NewUploadFileUseCase)
+	// handler...
+	_ = c.Provide(handler.NewUploadFileHandlerImpl)
+	// router...
+	_ = c.Provide(router.NewV1Router)
+	// server...
 	_ = c.Provide(server.NewAPIServer)
 }
 
