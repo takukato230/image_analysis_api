ac1eecc58d94c885834b264ce7071400ba05f515
takumi katori <>
2020-10-19 12:01:45 +0900

    add analysis service

--- domain/service/file_loading_service.go
+++ domain/service/file_loading_service.go
@@ -2,16 +2,20 @@ package service
 
 import (
 	"fmt"
+	"io/ioutil"
+
+	"github.com/takutakukatokatojapan/image_analysis_api/domain/model"
 	"github.com/takutakukatokatojapan/image_analysis_api/infrastructure/appctx"
 	"github.com/takutakukatokatojapan/image_analysis_api/infrastructure/logger"
-	"io/ioutil"
 )
 
 type (
 	FileLoadingService interface {
 		Load(ctx *appctx.APPCtx) FileLoadingResultContainer
+		LoadImage(ctx *appctx.APPCtx) FileLoadingResultContainer
 	}
 	FileLoadingServiceImpl struct {
+		ocr model.OCR
 	}
 	FileLoadingResultContainer struct {
 		FileName string
@@ -20,8 +24,8 @@ type (
 	}
 )
 
-func NewFileLoadingServiceImpl() FileLoadingService {
-	return &FileLoadingServiceImpl{}
+func NewFileLoadingServiceImpl(ocr model.OCR) FileLoadingService {
+	return &FileLoadingServiceImpl{ocr: ocr}
 }
 
 func (f FileLoadingServiceImpl) Load(ctx *appctx.APPCtx) FileLoadingResultContainer {
@@ -57,6 +61,49 @@ func (f FileLoadingServiceImpl) Load(ctx *appctx.APPCtx) FileLoadingResultContai
 	return FileLoadingResultContainer{
 		FileName: file.Filename,
 		Data:     string(b),
+		Error:    nil,
+	}
+}
+
+func (f FileLoadingServiceImpl) LoadImage(ctx *appctx.APPCtx) FileLoadingResultContainer {
+	file, err := ctx.FormFile("image")
+	if err != nil {
+		return FileLoadingResultContainer{
+			FileName: "",
+			Data:     "",
+			Error:    fmt.Errorf("happen where read parameter: %w", err),
+		}
+	}
+	fileBinary, err := file.Open()
+	if err != nil {
+		return FileLoadingResultContainer{
+			FileName: "",
+			Data:     "",
+			Error:    fmt.Errorf("happen where open file: %w", err),
+		}
+	}
+	defer func() {
+		_ = fileBinary.Close()
+	}()
+	b, err := ioutil.ReadAll(fileBinary)
+	if err != nil {
+		return FileLoadingResultContainer{
+			FileName: "",
+			Data:     "",
+			Error:    fmt.Errorf("happen when read file: %w", err),
+		}
+	}
+	o, err := f.ocr.Read(b)
+	if err != nil {
+		return FileLoadingResultContainer{
+			FileName: "",
+			Data:     "",
+			Error:    err,
+		}
+	}
+	return FileLoadingResultContainer{
+		FileName: file.Filename,
+		Data:     o,
 		Error:    nil,
 	}
 }
